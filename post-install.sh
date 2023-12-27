3ef758a69d7ce3747125a513c5ba31dc4914c69d
Martin Butt <me@martinbutt.com>
2023-12-27 09:56:02 -0800

    Move to persistent storage

--- post-install.sh
+++ post-install.sh
@@ -5,5 +5,5 @@ ln -s /usr/bin/e2fsck /usr/bin/fsck.ext2
 ln -s /usr/bin/e2fsck /usr/bin/fsck.ext3
 ln -s /usr/bin/e2fsck /usr/bin/fsck.ext4
 ln -s /usr/bin/e2fsck /usr/bin/fsck.ext4dev
-echo "Uninstall complete"
+echo "Post install complete"
 
